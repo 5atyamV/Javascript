56d153e9788b6ab272dab3edc9bd96e607b8187e
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-09 20:47:08 +0000

    Cleared Baiscs

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 145698723
+let accountEmail = "satyam123@google.com"
+var accountPassword = "456982"
+accountCity = "Noida" // this is also possile but never do this it
+
+accountEmail = "satyam123@sat.com"
+ accountPassword = "010101"
+accountCity = "SW"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+- let and Var will we mostly be used 
+- Semicolon at the end is icchaanusaar
+- Prefer not to use var Because of issue of block scope and functional Scope
+*/
