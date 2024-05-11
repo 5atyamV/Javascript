1754e41444959093cdb23dc0a617bd28e83f5fef
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-11 10:25:34 +0000

    comparisons

---
+++ 01_Basics/04_Comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// we avoid these kinds of comparions(Below) as they create lot of confusion 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
