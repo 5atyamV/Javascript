7424c82478d41028c79f8eba7ffcbe6464c4d94d
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-12 15:56:28 +0000

    memory

---
+++ 01_Basics/StackAndHeap_memory.js
@@ -0,0 +1,23 @@
+// Stack - Primitive 
+// Heap - No primitive
+
+let myname = "Satyam"
+
+let newname = myname
+ newname = "coder"
+ 
+ console.log(myname);
+ console.log(newname);
+
+ let useOne = {
+      userEmal: "Satyam@google.com"
+      
+ }
+
+ let userTwo = useOne
+
+ userTwo.Emal = "satyam@gmal.com"
+ 
+console.log(userOne.Emal);
+console.log(userTwo.Emal);
+
