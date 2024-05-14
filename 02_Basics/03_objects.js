9010d1921326b9f7d07f42a48efab4b0715bf5cd
SATYAM <133624894+5atyamV@users.noreply.github.com>
2024-05-14 18:26:35 +0000

    objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,38 @@
+// literal ki trh declare krte hai o singleton nhi banta hai But constructor se banega to singleton banega .
+
+//Object.create
+
+// Objects literal 
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
